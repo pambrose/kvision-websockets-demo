430e9e02bd8969e37f2d1e363f81856159462398
Paul Ambrose <pambrose@mac.com>
2023-06-01 18:01:04 -0700

    Clean up disconnects

--- src/backendMain/kotlin/com/github/pambrose/kvision_websockets/WsServiceImpl.kt
+++ src/backendMain/kotlin/com/github/pambrose/kvision_websockets/WsServiceImpl.kt
@@ -2,6 +2,7 @@ package com.github.pambrose.kvision_websockets
 
 import com.google.inject.Inject
 import io.ktor.server.websocket.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.SendChannel
 
@@ -10,7 +11,12 @@ actual class WsService : IWsService {
   @Inject
   lateinit var wsSession: WebSocketServerSession
 
+  @OptIn(ExperimentalCoroutinesApi::class)
   override suspend fun wsService(input: ReceiveChannel<Int>, output: SendChannel<String>) {
+    output.invokeOnClose {
+      println("WebSocket connection closed")
+    }
+
     for (i in input) {
       output.send("Got back: $i")
     }
